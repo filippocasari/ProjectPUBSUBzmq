11322e97f73793c6fb1ff49186a778a49983edcd
filippocasari <fili9941@gmail.com>
2021-07-22 15:55:08 +0200

    new features

--- ScriptAnalysis_2.py
+++ ScriptAnalysis_2.py
@@ -20,31 +20,31 @@ for i in range(num_experiments):
 print(dataframe_)
 fig, ax = plt.subplots(1, 1, figsize=(13, 8))
 j = 0
-x_scarti = [-1, 0.0, 1, 2]
+x_scrap = [-1, 0.0, 1, 2]
 range_payloads = [10, 50, 200, 1000]
 for payload in range_payloads:
     msg_rate_5 = dataframe_.loc[(dataframe_['message rate'] == 5) & (dataframe_['payload size'] == payload)]
-    msg_rate_5_mean = msg_rate_5['value'].mean()
-    msg_rate_5_std = msg_rate_5['value'].std()
+    msg_rate_5_mean = msg_rate_5['value'].mean() / 1000.0
+    msg_rate_5_std = msg_rate_5['value'].std() / 1000.0
     print("mean for msg rate = 5 is : ", msg_rate_5_mean, " with std: ", msg_rate_5_std)
 
     msg_rate_10 = dataframe_.loc[(dataframe_['message rate'] == 10) & (dataframe_['payload size'] == payload)]
-    msg_rate_10_mean = msg_rate_10['value'].mean()
-    msg_rate_10_std = msg_rate_10['value'].std()
+    msg_rate_10_mean = msg_rate_10['value'].mean()/ 1000.0
+    msg_rate_10_std = msg_rate_10['value'].std()/ 1000.0
     print("mean for msg rate = 10 is : ", msg_rate_10_mean, " with std: ", msg_rate_10_std)
     msg_rate_25 = dataframe_.loc[(dataframe_['message rate'] == 25) & (dataframe_['payload size'] == payload)]
-    msg_rate_25_mean = msg_rate_25['value'].mean()
-    msg_rate_25_std = msg_rate_25['value'].std()
+    msg_rate_25_mean = msg_rate_25['value'].mean()/ 1000.0
+    msg_rate_25_std = msg_rate_25['value'].std()/ 1000.0
     print("mean for msg rate = 25 is : ", msg_rate_25_mean, " with std: ", msg_rate_25_std)
 
     msg_rate_50 = dataframe_.loc[(dataframe_['message rate'] == 50) & (dataframe_['payload size'] == payload)]
-    msg_rate_50_mean = msg_rate_50['value'].mean()
-    msg_rate_50_std = msg_rate_50['value'].std()
+    msg_rate_50_mean = msg_rate_50['value'].mean()/ 1000.0
+    msg_rate_50_std = msg_rate_50['value'].std()/ 1000.0
     print("mean for msg rate = 50 is : ", msg_rate_50_mean, " with std: ", msg_rate_50_std)
 
     msg_rate_100 = dataframe_.loc[(dataframe_['message rate'] == 100) & (dataframe_['payload size'] == payload)]
-    msg_rate_100_mean = msg_rate_100['value'].mean()
-    msg_rate_100_std = msg_rate_100['value'].std()
+    msg_rate_100_mean = msg_rate_100['value'].mean()/ 1000.0
+    msg_rate_100_std = msg_rate_100['value'].std()/ 1000.0
     print("mean for msg rate = 100 is : ", msg_rate_100_mean, " with std: ", msg_rate_100_std)
     # print(msg_rate_5)
 
@@ -53,18 +53,20 @@ for payload in range_payloads:
     delays = [msg_rate_5_mean, msg_rate_10_mean, msg_rate_25_mean, msg_rate_50_mean, msg_rate_100_mean]
     error = [msg_rate_5_std, msg_rate_10_std, msg_rate_25_std, msg_rate_50_std, msg_rate_100_std]
 
-    msg_rates_temp = np.array(msg_rates) + x_scarti[j]
-    ax.bar(msg_rates_temp, delays, yerr=error, align='center', alpha=0.5, ecolor='black', capsize=6, width=1)
+    msg_rates_temp = np.array(msg_rates) + x_scrap[j]
+    w=1
+    ax.bar(msg_rates_temp, delays, yerr=error, align='center', alpha=0.5, ecolor='black', capsize=6, width=w, label='payload: '+str(payload)+' bytes')
     j += 1
-    ax.set_ylabel('mean of delay [microseconds]')
+    ax.set_ylabel('Average of delays [milliseconds]')
     ax.set_xticks(msg_rates)
     ax.set_xticklabels(msg_rates)
     ax.set_xlabel('message rate [msg/sec]')
-    ax.set_title('Mean of delays with standard deviation')
+    ax.set_title('Average of end to end delays\n with standard deviation')
     ax.yaxis.grid(True)
 
     plt.tight_layout()
     ax.autoscale(tight=True)
+    plt.ylim((0, 1.2))
     plt.xlim((-3, 120))
     # Save the figure and show
 plt.legend()
