849df5583627ea0123d0a159aa4c3aa9ab5006e2
filippocasari <fili9941@gmail.com>
2021-10-09 19:42:24 +0200

    change from 5 to 10 tests

--- Proxy.cpp
+++ Proxy.cpp
@@ -6,9 +6,9 @@ int main(){
     zactor_t *proxy = zactor_new (zproxy, NULL);
     zstr_sendx(proxy, "VERBOSE", NULL);
     zsock_wait(proxy);
-    zstr_sendx (proxy, "FRONTEND", "XSUB", ">tcp://169.254.124.70:5600", NULL);
+    zstr_sendx (proxy, "FRONTEND", "SUB", "@tcp://169.254.124.70:5600", NULL);
     zsock_wait (proxy);
-    zstr_sendx (proxy, "BACKEND", "XPUB", ">tcp://169.254.124.70:5602", NULL);
+    zstr_sendx (proxy, "BACKEND", "PUB", "@tcp://169.254.124.70:5602", NULL);
     zsock_wait (proxy);
 
     while(!zsys_interrupted and !zctx_interrupted){
